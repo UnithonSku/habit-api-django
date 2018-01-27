331dba99190b614083abca1497a5ae865fb6293b
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-01-27 17:44:45 +0900

    implemented todo CRUD

--- todo_api/admin.py
+++ todo_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from todo_api.models import *
 
 # Register your models here.
+admin.site.register(UserModel)
+admin.site.register(TodoModel)
+admin.site.register(CharacterModel)
