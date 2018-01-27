522b92b873cbb53df6a62f4b7f13f1810c9751f8
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-01-27 11:46:12 +0900

    Issue #2 - routing implemented

---
+++ todo_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from todo_api.views import *
+
+urlpatterns = [
+    path('', TodoView.as_view()),
+    path('create/', TodoCreateView.as_view()),
+    path('update/', TodoUpdateView.as_view()),
+    path('delete/', TodoDeleteView.as_view())
+]
